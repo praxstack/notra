52b3eb87b935e4f5b970a2aaa27943af66d9be33
Dominik K. <dominik@rivo.gg>
2026-02-24 22:46:13 +0100

    fix: update robots.ts

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/.well-known/**",
+      disallow: "/.well-known/",
     },
     sitemap: "https://www.usenotra.com/sitemap.xml",
   };
