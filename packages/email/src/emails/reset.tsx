0c1e92f54b7414210f3569364bf62236773d6102
Dominik K. <dominik@rivo.gg>
2026-04-07 21:49:42 +0200

    chore(email): clean up knip issues (#241)

--- packages/email/src/emails/reset.tsx
+++ packages/email/src/emails/reset.tsx
@@ -70,5 +70,3 @@ export const ResetPasswordEmail = ({
     </Html>
   );
 };
-
-export default ResetPasswordEmail;
--- packages/email/src/emails/welcome.tsx
+++ packages/email/src/emails/welcome.tsx
@@ -56,5 +56,3 @@ export const WelcomeEmail = () => {
     </Html>
   );
 };
-
-export default WelcomeEmail;
